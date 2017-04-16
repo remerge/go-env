f11705ec3a4c7a6691b035ff5981a4e2b55ff7a3
Benedikt Böhm <bb@xnull.de>
2017-04-16 16:18:54 +0000

    fix linter warnings

--- env.go
+++ env.go
@@ -6,8 +6,10 @@ import (
 	"github.com/bobziuchkovski/cue"
 )
 
+// Key is the OS environment variable that holds the environment name.
 const Key = "GO_ENV"
 
+// Env is the Go variable that holds the environment name.
 var Env string
 
 var log = cue.NewLogger("env")
@@ -16,6 +18,7 @@ func init() {
 	Set(os.Getenv(Key))
 }
 
+// Set the environment.
 func Set(name string) {
 	if name == "" {
 		name = "development"
@@ -33,14 +36,18 @@ func Set(name string) {
 	}
 }
 
+// IsProd returns true if the current environment is `production` or `prod`.
 func IsProd() bool {
 	return Env == "production" || Env == "prod"
 }
 
+// IsDevel returns true if the current environment is `development`, `devel` or
+// `dev`.
 func IsDevel() bool {
 	return Env == "development" || Env == "devel" || Env == "dev"
 }
 
+// IsTesting returns true if the current environment is `testing` or `test`.
 func IsTesting() bool {
 	return Env == "testing" || Env == "test"
 }
